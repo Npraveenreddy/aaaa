0bbcdbfe26739a107234fc89f69c577c2e676e46
PraveenReddy I <narayana.praveen143@gmail.com>
2022-05-02 11:06:34 +0530

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "onebucket" {
+   bucket = "delete-100"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "Bucket1"
+     Environment = "Test"
+   }
+}
