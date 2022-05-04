b989f44ce286b8c66a2c6c67d6c59ec7f579a9c6
PraveenReddy I <narayana.praveen143@gmail.com>
2022-05-04 15:14:54 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,23 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "delete-100"
-   acl = "private"
-   versioning {
+   bucket = "delete-1010"
+     versioning {
       enabled = true
    }
    tags = {
      Name = "Bucket1"
      Environment = "Test"
    }
+}
+terraform {
+
+  backend "s3" {
+
+    bucket = "delete-1010"
+
+    key    = "terraform.tfstate"
+
+    region = "us-east-2"
+
+  }
+
 }
